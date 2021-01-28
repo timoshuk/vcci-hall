7d5c3a7563c14b2397a43df14ffe0eff99825adc
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2021-01-28 16:15:24 +0200

    add variant page

--- pages/Theatre.jsx
+++ pages/Theatre.jsx
@@ -3,7 +3,7 @@ import Layout from "../components/layout"
 const Theatre = ()=>{
     return(
         <Layout head="Вигляд Theatre">
-
+            <h1>Theatre</h1>
         </Layout>
     )
 }
---
+++ pages/Version.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/layout"
+
+const Version = ()=>{
+    return(
+        <Layout head="Варіанти компоновки залу">
+
+        </Layout>
+    )
+}
+
+
+export default Version;
